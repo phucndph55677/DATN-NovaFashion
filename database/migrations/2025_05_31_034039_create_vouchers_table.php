9dea4d373b7bea663dadd8e7a364dc31722a674d
3107.Pucc <ndp112005@gmail.com>
2025-06-24 11:15:40 +0700

    fix(db vouchers)

--- database/migrations/2025_05_31_034039_create_vouchers_table.php
+++ database/migrations/2025_05_31_034039_create_vouchers_table.php
@@ -13,14 +13,16 @@ return new class extends Migration
     {
         Schema::create('vouchers', function (Blueprint $table) {
             $table->id(); // Khóa chính id
-            $table->foreignId('role_id')->constrained()->onDelete('cascade'); // Khóa ngoại liên kết tới bảng roles
-            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Khóa ngoại liên kết tới bảng users
-            $table->string('name');          // Tên voucher
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('set null');
             $table->string('voucher_code')->unique(); // Mã voucher, duy nhất
-            $table->integer('quantity')->default(0);  // Số lượng voucher còn lại
+            $table->integer('quantity')->default(0);  // Số lượng voucher được tung ra
+            $table->integer('total_used')->default(0); // Số lượng voucher đã sử dụng
+            $table->integer('user_limit')->default(1); // Số lần 1 user được sử dụng
             $table->decimal('sale_price', 10, 2);     // Giá trị giảm giá
             $table->decimal('min_price', 10, 2);      // Giá tối thiểu đơn hàng được áp dụng voucher
             $table->decimal('max_price', 10, 2)->nullable(); // Giá tối đa được giảm (có thể null)
+            $table->tinyInteger('status')->default(1); // 1: active, 0: inactive
+            $table->text('description')->nullable();   // Mô tả chi tiết voucher
             $table->dateTime('start_date');      // Ngày bắt đầu áp dụng voucher (có giờ)
             $table->dateTime('end_date');        // Ngày kết thúc áp dụng voucher (có giờ)
             $table->timestamps(); // Tạo 2 cột created_at và updated_at
