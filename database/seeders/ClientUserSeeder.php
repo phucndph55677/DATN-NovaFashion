6296fc0b8ec54a8c8285c3400d74fcf7b2ca4027
Nguyen Phat <drawtecu@gmail.com>
2025-07-16 13:11:35 +0700

    feat(seeder ClientUserSeeder)

---
+++ database/seeders/ClientUserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+
+class ClientUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'role_id'        => 3, // role client
+            'ranking_id'     => null, // hoặc số id ranking nếu bạn có
+            'image'          => null,
+            'name'           => 'Client',
+            'phone'          => '0987654321',
+            'email'          => 'client@example.com',
+            'password'       => Hash::make('12345678'),
+            'address'        => 'TP. Hồ Chí Minh',
+            'is_verified'    => true,
+            'remember_token' => Str::random(10),
+            'created_at'     => now(),
+            'updated_at'     => now(),
+        ]);
+    }
+}
