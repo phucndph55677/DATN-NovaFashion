b538222a7773ed52f189cf79dccd20c77ba1c497
Văn Đạt <levandat21082005@gmail.com>
2025-08-02 14:37:31 +0700

    feat(db payment_statuses)

---
+++ database/seeders/PaymentStatusSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\PaymentStatus;
+
+class PaymentStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $statuses = [
+            'Chưa thanh toán',
+            'Đã thanh toán',
+            'Hoàn tiền',
+        ];
+
+        foreach ($statuses as $status) {
+            PaymentStatus::create(['name' => $status]);
+        }
+    }
+}
