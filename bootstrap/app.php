041febfd0c9a799e731c2dff565fdd1d8a30e5a9
Dang Vinh <vinhd1044@gmail.com>
2025-08-19 01:47:01 +0700

    fix/routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,10 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        web: [
+            __DIR__.'/../routes/admin.php',
+            __DIR__.'/../routes/client.php',
+        ],
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
