908e20af559df7f4c0fcd84b42939fe1e2ed0811
Dang Vinh <vinhd1044@gmail.com>
2025-08-23 23:10:39 +0700

    fix/client/footer

--- resources/views/client/partials/footer.blade.php
+++ resources/views/client/partials/footer.blade.php
@@ -3,57 +3,10 @@
     <div class="container">
         <div class="main-footer d-flex">
             <div class="left-footer">
-                <div class="top-left">
-                    <div class="logo-footer">
-                        <a href="https://ivymoda.com/">
-                            <img src="https://pubcdn.ivymoda.com/ivy2/images/logo-footer.png" alt="logo-footer"
-                                class="lazy" loading="lazy" />
-                        </a>
-                    </div>
-                    <a href="//www.dmca.com/Protection/Status.aspx?ID=0cfdeac4-6e7f-4fca-941f-57a0a0962777"
-                        target="_blank" title="DMCA.com Protection Status" class="dmca-badge">
-                        <img src ="https://pubcdn.ivymoda.com/ivy2/images/dmca.png"
-                            alt="DMCA.com Protection Status" class="lazy" loading="lazy" />
-                    </a>
-                    <div class="logo-conthuong">
-                        <a target="_blank" href="http://online.gov.vn/Home/WebDetails/36596">
-                            <img src="https://pubcdn.ivymoda.com/ivy2/images/img-congthuong.png"
-                                alt="img-congthuong" class="lazy" loading="lazy" />
-                        </a>
-                    </div>
-                </div>
+                <img src="{{ asset('storage/logo/logo_nf_hcn.png') }}" style="width: 230px; height: 180px;">
                 <div class="content-left-ft content-ft">
-                    <div class="info-left-ft">
-                    </div>
-                    <ul class="list-social">
-                        <li>
-                            <a href="https://www.facebook.com/thoitrangivymoda/" target="_blank">
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png" alt="ic_fb" class="lazy" loading="lazy" style="width:24px;" />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://ivymoda.com/" target="_blank">
-                                <img src="https://pubcdn.ivymoda.com/ivy2/images/ic_gg.svg" alt="ic_gg" class="lazy" loading="lazy" />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.instagram.com/ivy_moda/" target="_blank">
-                                <img src="https://pubcdn.ivymoda.com/ivy2/images/ic_instagram.svg" alt="ic_instagram" style="height: 28px;" class="lazy" loading="lazy" />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.pinterest.com/ivymoda/_saved/" target="_blank">
-                                <img src="https://pubcdn.ivymoda.com/ivy2/images/ic_pinterest.svg" alt="ic_pinterest" style="height: 27px" class="lazy" loading="lazy" />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://zalo.me/2129412421626719329">
-                                <img src="https://pubcdn.ivymoda.com/ivy2/images/ic_zalo.png" alt="ic_zalo" style="height: 28px;" class="lazy" loading="lazy">
-                            </a>
-                        </li>
-                    </ul>
                     <div class="hotline">
-                        <a href="tel:02466623434">Hotline: 0899505715</a>
+                        <a href="tel:0899505715">Hotline: 0899505715</a>
                     </div>
                 </div>
             </div>
@@ -62,44 +15,30 @@
                     <p class="title-footer">Giới thiệu</p>
                     <ul>
                         <li>
-                            <a href="https://ivymoda.com/about/gioi-thieu">Về IVY moda</a>
+                            <a href="#">Về NovaFashion</a>
                         </li>
                         <li>
-                            <a target="_blank" href="https://tuyendung.ivy.com.vn">Tuyển dụng</a>
+                            <a href="#">Tuyển dụng</a>
                         </li>
                         <li>
-                            <a href="https://ivymoda.com/page/cuahang">Hệ thống cửa hàng</a>
+                            <a href="#">Hệ thống cửa hàng</a>
                         </li>
                     </ul>
                 </div>
                 <div class="main-center-ft item-center-ft">
-                    <p class="title-footer">Dịch vụ khách hàng</p>
+                    <p class="title-footer">Dịch vụ</p>
                     <ul>
                         <li>
-                            <a href="https://ivymoda.com/about/chinhsach-dieukhoan">Chính sách điều khoản</a>
+                            <a href="#">Chính sách điều khoản</a>
                         </li>
                         <li>
-                            <a href="https://ivymoda.com/about/huong-dan-mua-hang">Hướng dẫn mua hàng</a>
+                            <a href="#">Hướng dẫn mua hàng</a>
                         </li>
                         <li>
-                            <a href="https://ivymoda.com/about/chinh-sach-thanh-toan">Chính sách thanh toán</a>
+                            <a href="#">Chính sách thanh toán</a>
                         </li>
                         <li>
-                            <a href="https://ivymoda.com/about/chinh-sach-doi-tra">Chính sách đổi trả</a>
-                        </li>
-                        <li>
-                            <a href="https://ivymoda.com/about/chinh-sach-bao-hanh">Chính sách bảo hành</a>
-                        </li>
-                        <li>
-                            <a href="https://ivymoda.com/about/chinh-sach-giao-nhan-van-chuyen">Chính sách giao
-                                nhận vận chuyển</a>
-                        </li>
-                        <li>
-                            <a href="https://ivymoda.com/about/chinh-sach-the-thanh-vien">Chính sách thẻ thành
-                                viên</a>
-                        </li>
-                        <li>
-                            <a href="https://ivymoda.com/page/cuahang">Hệ thống cửa hàng</a>
+                            <a href="#">Chính sách bảo hành</a>
                         </li>
                     </ul>
                 </div>
@@ -107,55 +46,41 @@
                     <p class="title-footer">Liên hệ</p>
                     <ul>
                         <li>
-                            <a href="tel:02466623434">Hotline</a>
+                            <a href="https://www.facebook.com/novafashion.fanpage">Facebook</a>
                         </li>
                         <li>
-                            <a href="mailto:saleadmin@ivy.com.vn">Email</a>
+                            <a href="https://m.me/novafashion.fanpage">Messenger</a>
                         </li>
                         <li>
-                            <a href="javascript:openCsChatBox();">Live Chat</a>
+                            <a href="#">Liên hệ: 0899505715</a>
                         </li>
                         <li>
-                            <a href="http://messenger.com/t/thoitrangivymoda" target="_blank">Messenger</a>
-                        </li>
-                        <li>
-                            <a href="https://ivymoda.com/lien-he">Liên hệ</a>
+                            <a href="#">Email: novafashion.contact.us</a>
                         </li>
                     </ul>
                 </div>
             </div>
             <div class="right-footer">
                 <div class="register-form">
-                    <p class="title-footer">Nhận thông tin các chương trình của NF</p>
+                    <p class="title-footer">Nhận thông tin các chương trình của NovaFashion</p>
                     <form id="frm_subscribe">
                         <input id="email_subscribe" type="text" name="email"
                             placeholder="Nhập địa chỉ email" required="required">
                         <div class="btn-submit">
-                            <input id="btn-submit" class="form-submit" value="Đăng ký" type="submit" />
+                            <input class="form-submit" value="Đăng ký" type="submit" />
                         </div>
                     </form>
                     <div id="subscribe_error"></div>
-                </div>
-                <div class="info-right-ft">
-                    <p class="title-footer">Download App</p>
-                    <ul>
-                        <li>
-                            <a id="app_ios" href="http://ios.ivy.vn" class="link-white" target="_blank"
-                                title="Tải App IVYmoda trên App Store">
-                                <img src="https://pubcdn.ivymoda.com/ivy2/images/appstore.png"
-                                    class="img-fluid lazy" alt="" loading="lazy" />
-                            </a>
-                        </li>
-                    </ul>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 <!-- site-footer -->
 <footer id="footer" class="site-footer">
 	<div class="container">
-		<div class="coppy-right">©NOva All rights reserved</div>
+		<div class="coppy-right">Website bán quần áo thời trang NovaFashion</div>
 	</div>
 	<!-- .container -->
 </footer>
