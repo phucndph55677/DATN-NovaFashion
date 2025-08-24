f88c47aa769d5b6f100850e8fc1be4fd18370d3d
Dang Vinh <vinhd1044@gmail.com>
2025-08-24 23:57:25 +0700

    format/admin/partials

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -2,16 +2,14 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-6">
-                <ul class="list-inline mb-0">
+                {{-- <ul class="list-inline mb-0">
                     <li class="list-inline-item"><a href="https://templates.iqonic.design/datum-dist/laravel/public/privacypolicy">Privacy Policy</a></li>
                     <li class="list-inline-item"><a href="https://templates.iqonic.design/datum-dist/laravel/public/termsofservice">Terms of Use</a></li>
-                </ul>
+                </ul> --}}
             </div>
             <div class="col-lg-6 text-lg-end text-center mt-lg-0 mt-3">
                 <span class="mr-1">
-                    © Copyright&nbsp;
-                    <script>document.write(new Date().getFullYear())</script>&nbsp;<a href="#" class="">NovaFashion</a> |
-                    All Rights Reserved.
+                    <script>document.write(new Date().getFullYear())</script>&nbsp;<a href="https://www.facebook.com/novafashion.fanpage" class="">Website bán quần áo thời trang NovaFashion</a>
                 </span>
             </div>
         </div>
