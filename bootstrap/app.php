c4612aed5c5a16416a4bae63938a123debbe2c3f
3107.Pucc <ndp112005@gmail.com>
2025-09-02 08:34:56 +0700

    Setup Pusher for Laravel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
             __DIR__.'/../routes/admin.php',
             __DIR__.'/../routes/client.php',
         ],
+        channels: __DIR__.'/../routes/channels.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="N4bfmjN5LEW5HT2ziKoPvKUNnsQCty6ldtdDitFK">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'NovaFashion | CRM Admin Dashboard')</title>
 
